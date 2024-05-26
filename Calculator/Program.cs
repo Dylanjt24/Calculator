c7c2872c7e41748b6310bd91ac0db35af7e9d0d7
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-05-26 14:26:37 -0700

    Added logic for user to input math numbers and choose operator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -34,6 +34,46 @@ class Program
         // Intitial calculator title
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
-        return;
+
+        while (!endApp)
+        {
+            // Declare intial empty math variables
+            string? numInput1 = "";
+            string? numInput2 = "";
+            double result = 0;
+
+            // Ask user for first math number
+            Console.WriteLine("Type a number, then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            // Prompt for input until a number is input, save it in cleanNum1
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.WriteLine("This is not a valid input. Please enter a numeric value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            // Ask user for second math number
+            Console.WriteLine("Type a number, then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.WriteLine("This is not a valid input. Please enter a numeric value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            // Prompt user to choose math operator
+            Console.WriteLine("Choose an operator from the following list:\n" +
+                "\ta - Add\n" +
+                "\ts - Subtract\n" +
+                "\tm - Multiply\n" +
+                "\td - Divide\n" +
+                "Your choice? ");
+
+            string? op = Console.ReadLine();
+        }
     }
 }
