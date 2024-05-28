b1db8ccf0b4c43c856cf87a5c2ca4b8fbd79b5c2
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-05-28 09:24:42 -0700

    Moved Calculator class from Program.cs to CalculatorLibrary.cs

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,30 @@
+﻿namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not a number" to avoid division by 0 errors
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num2 - num1;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num1 != 0) result = num1 / num2;
+                    break;
+                // Return text displaying an incorrect option was input
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
