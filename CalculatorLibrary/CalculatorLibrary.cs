bc81f75b35f3a0ebc955650e2dc04fda13f02368
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-06-02 00:20:34 -0700

    Removed old trace code

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -5,44 +5,59 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        JsonWriter writer;
         public Calculator()
         {
-            StreamWriter logFile = File.CreateText("calculator.log"); // StreamWriter implements a TextWriter for writing characters to a stream in a particular encoding
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile)); // Adds a TraceListener and directs tracing/debugging to the created logFile
-            Trace.AutoFlush = true; // Calls Flush() after every write, which flushes the output buffer and causes buffered data to be written to the Listeners
-            Trace.WriteLine("Starting Calculator Log"); // Writes the value of the object's ToString() method to the Listeners
-            Trace.WriteLine(String.Format($"Started {System.DateTime.Now.ToString()}"));
+            StreamWriter logFile = File.CreateText("calculator.json"); // StreamWriter implements a TextWriter for writing characters to a stream in a particular encoding
+            logFile.AutoFlush = true; // Calls Flush() after every write, which flushes the output buffer and causes buffered data to be written
+            writer = new JsonTextWriter(logFile); // Represents a writer that provides a fast, non-cached, forward-only way of generating JSON data
+            writer.Formatting = Formatting.Indented; // Causes child objects to be indented
+            writer.WriteStartObject(); // Writes a JSON start object ({)
+            writer.WritePropertyName("Operations"); // Writes the property name of the name/value pair
+            writer.WriteStartArray(); // Writes a JSON start array ([)
         }
         public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not a number" to avoid division by 0 errors
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
 
+
+            // Switch statement to do the math
             switch (op)
             {
                 case "a":
                     result = num1 + num2;
-                    Trace.WriteLine($"{num1} + {num2} = {result}");
+                    writer.WriteValue("Add");
                     break;
                 case "s":
                     result = num2 - num1;
-                    Trace.WriteLine($"{num1} - {num2} = {result}");
+                    writer.WriteValue("Subtract");
                     break;
                 case "m":
                     result = num1 * num2;
-                    Trace.WriteLine($"{num1} * {num2} = {result}");
+                    writer.WriteValue("Multiply");
                     break;
                 case "d":
                     // Make sure divisor is non-zero
                     if (num2 != 0)
                     {
                         result = num1 / num2;
-                        Trace.WriteLine($"{num1} / {num2} = {result}");
+                        writer.WriteValue("Divide");
                     }
                     break;
                 // Return text displaying an incorrect option was input
                 default:
                     break;
             }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject(); // Writes a JSON end object (})
+
             return result;
         }
     }
